3840ccc82063e34934f6ab1eb79b6b0c6695cf52
Alexandr Tarelkin <bulbozaur@users.noreply.github.com>
2018-09-21 10:46:02 +0300

    Adding 2 fingers swipe

--- lib/fusuma/gesture_event.rb
+++ lib/fusuma/gesture_event.rb
@@ -15,20 +15,21 @@ module Fusuma
     class << self
       def initialize_by(line, device_names)
         return if device_names.none? do |device_name|
-          line =~ /^\s?#{device_name}/
+          line.to_s =~ /^\s?#{device_name}/
         end
-        return if line =~ /_BEGIN/
-        return unless line =~ /GESTURE_SWIPE|GESTURE_PINCH/
+        return if line.to_s =~ /_BEGIN/
+        return unless line.to_s =~ /GESTURE_SWIPE|GESTURE_PINCH|POINTER_AXIS/
         time, event, finger, directions = gesture_event_arguments(line)
         MultiLogger.debug(time: time, event: event,
                           finger: finger, directions: directions)
-        new(time, event, finger, directions)
+        GestureEvent.new(time, event, finger, directions)
       end
 
       private
 
       def gesture_event_arguments(libinput_line)
         event, time, finger, other = parse_libinput(libinput_line)
+        puts "===============", other
         move_x, move_y, zoom = parse_finger_directions(other)
         directions = { move: { x: move_x, y: move_y }, zoom: zoom }
         [time, event, finger, directions]
@@ -36,12 +37,23 @@ module Fusuma
 
       def parse_libinput(line)
         _device, event, time, other = line.strip.split(nil, 4)
-        finger, other = other.split(nil, 2)
+        if event === 'POINTER_AXIS'
+          finger = 2
+          move_x, move_y = other.gsub('horiz','').gsub('vert', '').gsub('*','').gsub('(finger)','').split
+          other = "#{move_y}/#{move_x}"
+          if move_x === '0.00' && move_y === '0.00'
+            event = 'GESTURE_SWIPE_END'
+          else
+            event = 'GESTURE_SWIPE_UPDATE'
+          end
+        else
+          finger, other = other.split(nil, 2)
+        end
         [event, time, finger, other]
       end
 
       def parse_finger_directions(line)
-        return [] unless line
+        return [] if line.nil?
         move_x, move_y, _, _, _, zoom = line.tr('/|(|)', ' ').split
         [move_x, move_y, zoom]
       end
